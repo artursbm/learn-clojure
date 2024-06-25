55f66cef09b6ce70db3cc8d3b5c9b0d182497034
Artur mello <artursbmello@gmail.com>
2024-06-25 20:13:45 -0300

    fixing print-name call

--- learning-clj/src/learning_clj/core.clj
+++ learning-clj/src/learning_clj/core.clj
@@ -1,11 +1,11 @@
 (ns learning-clj.core
   (:gen-class)
   (:require [cider.nrepl :refer (cider-nrepl-handler)]
-            [learning-clj.tutorial :as tutorial])
+            [learning-clj.tutorial :as tutorial]))
 
 ; TODO check how to make the REPL start from the project main
 ; [nrepl.server :as nrepl-server])
 
-  (defn -main
-    "I don't do a whole lot ... yet."
-    [] (tutorial/print-name))
+(defn -main
+  "I don't do a whole lot ... yet."
+  [] (tutorial/print-name "Hey, there, "))
